be0fb41548969b74b950b99131680651543502cf
fwplove <625881050@qq.com>
2019-10-12 09:11:01 +0800

    cmspage

---
+++ src/module/cms/page/page_list.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!--相当于编写html的内容-->
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+//填写js代码，实现VM的功能，创建vue实例
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style>
+/*css样式*/
+</style>
