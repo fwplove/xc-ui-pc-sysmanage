dde1fa9f17e46ecf4495ce16d9adffc64b6909f6
fwplove <625881050@qq.com>
2020-01-07 23:47:50 +0800

    s

--- src/module/cms/page/page_list.vue
+++ src/module/cms/page/page_list.vue
@@ -1,41 +1,64 @@
+ 
 <template>
   <div>
-    <!--相当于编写html的内容-->
-    <el-table :data="tableData" stripe style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
-    </el-table>
+    <el-button type="primary" v-on:click="query"  size="small">查询</el-button>
+  <el-table
+    :data="list"
+    stripe
+    style="width: 100%">
+    <el-table-column type="index" width="60">
+    </el-table-column>
+    <el-table-column prop="pageName" label="页面名称" width="120">
+    </el-table-column>
+    <el-table-column prop="pageAliase" label="别名" width="120">
+    </el-table-column>
+    <el-table-column prop="pageType" label="页面类型" width="150">
+    </el-table-column>
+    <el-table-column prop="pageWebPath" label="访问路径" width="250">
+    </el-table-column>
+    <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
+    </el-table-column>
+    <el-table-column prop="pageCreateTime" label="创建时间" width="180" >
+    </el-table-column>
+  </el-table>
+    <el-pagination
+      layout="prev, pager, next"
+      :page-size="this.params.size"
+      v-on:current-change="changePage"
+      :total="total" :current-page="this.params.page" style="float:right;">
+    </el-pagination>
   </div>
 </template>
 <script>
-//填写js代码，实现VM的功能，创建vue实例
+import * as cmsApi from "../api/cms";
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      list: [],
+      total: 50,
+      params: {
+        page: 1, //页码
+        size: 2 //每页显示个数
+      }
     };
+  },
+  methods: {
+    //分页查询
+    changePage: function() {
+      this.query();
+    },
+
+    //查询
+    query: function() {
+      cmsApi
+        .page_list(this.params.page, this.params.size, this.params)
+        .then(res => {
+          console.log(res);
+          this.total = res.queryResult.total;
+          this.list = res.queryResult.list;
+        });
+    }
+
   }
 };
 </script>
